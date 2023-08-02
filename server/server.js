a12bf66351b8fd07b7a75312e7ec14df82508fb2
mlgibson8 <michael.l.gibson@du.edu>
2023-08-02 13:27:13 -0600

    updates

--- server/server.js
+++ server/server.js
@@ -11,17 +11,25 @@ const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
-;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+
+;
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build'));
+});
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
@@ -30,14 +38,9 @@ const startApolloServer = async (typeDefs, resolvers) => {
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   })
   });
-server.applyMiddleware({ app })
+
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-}
+
 };
 //app.use(routes);
 
