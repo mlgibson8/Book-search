0227d0bea0da2d865b739e11eb8e511d0f757f38
mlgibson8 <michael.l.gibson@du.edu>
2023-08-02 15:48:32 -0600

    atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mlgibson8:abcdefg@cluster0.5a988ya.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
